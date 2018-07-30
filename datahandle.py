e5250e4235f610383a80bd47cfb86670814aa75b
KenanBolat <kenan23@gmail.com>
2018-07-30 12:52:57 +0300

    Seconde Commit

--- datahandle.py
+++ datahandle.py
@@ -6,15 +6,18 @@ class Handle:
     def __init__(self):
         pass
 
-    def get_file_tags(self, filename, delimiter="_"):
+    @staticmethod
+    def get_file_tags(filename, delimiter="_"):
         file_tag = os.path.splitext(os.path.basename(filename))[0]
         date_tag = file_tag.split(delimiter)
         return date_tag
 
-    def _check_file_exists(self, filename):
+    @staticmethod
+    def _check_file_exists(filename):
         return os.path.exists(filename)
 
-    def write_data(self,filename):
+    @staticmethod
+    def write_data(filename):
         """if self._check_file_exists(filename):
             print "there is a::::::::::::::: ", filename
         else:
--- main.py
+++ main.py
@@ -13,11 +13,11 @@ import datahandle
 start = datetime.datetime.now()
 print start
 
-
+LOG = []
 
 
 # Define Path
-data_path = r"I:\temp\DailyGroundObservations\DailyGroundObservations"
+data_path = r"/home/klm/Desktop/GroundData/DailyGroundObservations"
 merged_file = []
 
 # Trace each file and replace "|"  with tab and "NULL" with 9999
@@ -25,17 +25,40 @@ for r,d,f in os.walk(data_path):
     print d
     for i in f:
         filename = os.path.join(r, i)
+
         file_tags = datahandle.Handle.get_file_tags(filename)
+        print file_tags
         datahandle.Handle.write_data(file_tags[1])
+        with open(filename, 'rb') as csvfile:
+            spamreader = csv.reader(csvfile)
+            for row in spamreader:
+                with open(file_tags[1], 'a') as csvfile:
+                    if row[0].find("NULL") != -1:
+                        LOG.append(filename)
+                    a = row[0].replace("NULL","9999")
+                    if a.find("||") != -1 or a.find("CCA") or a.find("SCA"):
+                        LOG.append(filename)
+                    a = row[0].replace("NULL", "9999")
+                    a = a.replace("CAA", "")
+                    a = a.replace("CAA", "")
+                    a = a.replace("CAB", "")
+                    a = a.replace("CCB", "")
+                    a = a.replace("CCC", "")
+                    a = a.replace("CCD", "")
+                    a = a.replace("CCE", "")
+                    a = a.replace("CCF", "")
+                    a = a.replace("RAA", "")
+                    a = a.replace("RRA", "")
+                    a = a.replace("RRN", "")
+                    a = a.replace("VVU", "")
 
+                    a = a.replace("||", "|")
 
-
-
-
-
-
-
-
+                    spamwriter = csv.writer(csvfile)
+                    spamwriter.writerow([a])
 
 end = datetime.datetime.now()
 print end-start
+
+
+print "="*100
